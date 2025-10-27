036f5e081748f0b16450f4a2483ba5bde8e1e254
Milan Hauth <milahu@gmail.com>
2025-10-27 15:03:46 +0100

    remove PyWeakref_GetObject for python 3.12

--- src/python3/connection.c
+++ src/python3/connection.c
@@ -812,12 +812,22 @@ static void _pysqlite_drop_unused_statement_references(pysqlite_Connection* self
 
     for (i = 0; i < PyList_Size(self->statements); i++) {
         weakref = PyList_GetItem(self->statements, i);
-        if (PyWeakref_GetObject(weakref) != Py_None) {
-            if (PyList_Append(new_list, weakref) != 0) {
-                Py_DECREF(new_list);
-                return;
+        PyObject *obj;
+        int ok = PyWeakref_GetRef(weakref, &obj);
+        if (ok > 0) {
+            // weakref is alive
+            if (obj != Py_None) {
+                if (PyList_Append(new_list, weakref) != 0) {
+                    Py_DECREF(obj);
+                    Py_DECREF(new_list);
+                    return;
+                }
             }
+            // release strong reference
+            Py_DECREF(obj);
         }
+        // ok == 0 => weakref is dead, skip
+        // ok < 0 => error occurred, could log if desired
     }
 
     Py_DECREF(self->statements);
